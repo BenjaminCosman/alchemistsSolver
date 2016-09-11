d907a6453376e198e6853d29bd23f72b17a592a0
Rafael Cosman <rafaelcosman@gmail.com>
2016-09-10 20:04:34 -0700

    Adds example.js

---
+++ example.js
@@ -0,0 +1,8 @@
+var HelloMessage = React.createClass({
+  render: function() {
+    return <div>Hello {this.props.name}</div>;
+  }
+});
+
+ReactDOM.render(<HelloMessage name="John" />, mountNode);
+
