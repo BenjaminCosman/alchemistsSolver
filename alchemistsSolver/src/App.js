3c71375519a1b2820701d6eda96dfbcb9df246f8
Rafael Cosman <rafaelcosman@gmail.com>
2016-09-11 13:06:49 -0700

    Adds mix(), check(), and unit tests

--- alchemistsSolver/src/App.js
+++ alchemistsSolver/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-// var _ = require('lodash');
+import _ from 'lodash'
+
+import $ from 'jquery'
 
 var Fact = React.createClass({
   render: function() {
@@ -58,5 +60,90 @@ var TodoApp = React.createClass({
     return 3;
   }
 });
+
+var ColorMapping = {
+  0: "red",
+  1: "green",
+  2: "blue",
+};
+
+// var SignMapping = {
+//   -1: "minus",
+//   0: "neutral",
+//   1: "plus",
+// };
+
+// This is what an Alchemical looks like:
+// [1, 1, -1]
+
+// Here are all of the Alchemicals:
+var alchemicals = [
+  [-1, +1, -1],
+  [+1, -1, +1],
+  [+1, -1, -1],
+  [-1, +1, +1],
+  [-1, -1, +1],
+  [+1, +1, -1],
+  [-1, -1, -1],
+  [+1, +1, +1],
+]
+
+
+// Alchemical -> Alchemical -> Potion
+function mix(alchemicalA, alchemicalB) {
+  var mean = _.zipWith(alchemicalA, alchemicalB, function(a,b){return (a+b)/2})
+  for (var index = 0; index < 3; index++) {
+    if (mean[(index+1) % 3] !== 0)
+      mean[index] = 0
+  }
+
+  return mean
+}
+
+function check(world, fact) {
+  var alchemicalA = world[fact.ingredients[0]]
+  var alchemicalB = world[fact.ingredients[1]]
+  var result = mix(alchemicalA, alchemicalB)
+  console.log(result)
+  console.log(fact.possibleResults)
+  return fact.possibleResults.find(function(arr) {
+    return _.isEqual(arr, result)
+  }) != undefined
+}
+
+// ===== Unit tests =====
+
+// Here is what a World looks like:
+var exampleWorld = [
+  [+1, -1, -1],
+  [-1, +1, -1],
+  [+1, -1, +1],
+  [-1, +1, +1],
+  [+1, +1, +1],
+  [-1, -1, +1],
+  [+1, +1, -1],
+  [-1, -1, -1],
+]
+
+// This is what an Ingredient looks like:
+var exampleIngredientA = 4
+var exampleIngredientB = 5
+
+// This is what a Potion looks like:
+var examplePotionA = [0, 0, 1] // this is a blue+
+var examplePotionB = [0, 0, 0] // this is soup
+
+// This is what a Fact looks like:
+// {
+//   ingredients: [5, 7]
+//   possibleResults: [0, 0, 1], [0, 1, 0]]
+// }
+// combining ingredient 5 and 7 make either a green+ or a blue+
+var exampleFact = {
+  ingredients: [exampleIngredientA, exampleIngredientB],
+  possibleResults: [examplePotionA, examplePotionB]
+}
+
+console.log(check(exampleWorld, exampleFact))
 
 export default TodoApp;
