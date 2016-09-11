734f7bf82d941fe933f9868194aa5ffcc1ff2d3c
Rafael Cosman <rafaelcosman@gmail.com>
2016-09-11 10:51:20 -0700

    Moves data to root to be more react like

--- alchemistsSolver/src/App.js
+++ alchemistsSolver/src/App.js
@@ -6,47 +6,32 @@ import './App.css';
 
 var Fact = React.createClass({
   render: function() {
-    return <li key={this.props.item.id}>{this.props.item.text}</li>
+    return <li key={this.props.item.id}>{this.props.index}: {this.props.item.text}</li>
   }
-})
+});
 
 var TodoList = React.createClass({
-  getInitialState: function() {
-    return {facts: []}
-  },
-  addFact: function(fact) {
-    this.setState({facts: this.state.facts.concat([fact])});
-  },
   render: function() {
-    var createItem = function(item) {
-      return item;
-    };
-    return <ul>{this.state.facts}</ul>;
+    return <ul>{this.props.items.map((fact, factIndex) => <Fact item={fact} index={factIndex}/>)}</ul>;
   }
 });
 
 var TodoApp = React.createClass({
   getInitialState: function() {
-    // var foo = React.createElement(TodoList);
-    // console.log(foo);
-    // foo.addFact(null);
-    return {factlist: React.createElement(TodoList), text: ''};
+    return {factlist: [], text: ''};
   },
   onChange: function(e) {
     this.setState({text: e.target.value});
   },
   handleSubmit: function(e) {
     e.preventDefault();
-    console.log(this.state.factlist);
-    var nextfacts = this.state.factlist.addFact(React.createElement(Fact, {text: this.state.text, id: Date.now()}));
-    var nextText = '';
-    this.setState({factlist: nextfacts, text: nextText});
+    this.setState({factlist: this.state.factlist.concat([{text: this.state.text, id: Date.now()}]), text: ""});
   },
   render: function() {
     return (
       <div>
         <h3>TODO</h3>
-        {this.state.factlist}
+        <TodoList items={this.state.factlist}/>
         <form onSubmit={this.handleSubmit}>
           <input onChange={this.onChange} value={this.state.text} />
           <button>{'Add #' + (100)}</button>
