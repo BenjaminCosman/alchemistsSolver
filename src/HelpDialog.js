ed0949dfe13bc857b9b475ad52430d78779cb03a
Benjamin Cosman <bcosman@caltech.edu>
2016-09-15 23:02:05 -0700

    added Bayes mode to Help dialog

--- src/HelpDialog.js
+++ src/HelpDialog.js
@@ -35,6 +35,12 @@ class HelpDialog extends React.Component {
         the <a href="http://czechgames.com/en/alchemists/downloads/">rules of Alchemists</a>.)
         <br/>
         <br/>
+        <h4>Remaining Worlds:</h4>
+        Your world is described by the true mapping between ingredients and alchemicals.
+        At the beginning of the game any mapping is possible, so there are (8!) worlds you
+        could be in. This counter tracks how many are still possible.
+        <br/>
+        <br/>
         <h4>Two-Ingredient Facts:</h4>
         When you mix a potion, enter the results as a Two-Ingredient Fact. Select
         all potions that you might have made, e.g. if you try to sell a Red- to the
@@ -48,17 +54,19 @@ class HelpDialog extends React.Component {
         of a potion using Periscope, or for Apprentice-Variant debunking. For example,
         if an opponent mixes a "red or green plus" potion for the adventurer and you
         Periscope an ingredient, check off both red+ and green+.
-        <br/>
-        <br/>
-        <h4>Remaining Worlds:</h4>
-        Your world is described by the true mapping between ingredients and alchemicals.
-        At the beginning of the game any mapping is possible, so there are (8!) worlds you
-        could be in. This counter tracks how many are still possible.
+        <h5>Bayes Mode</h5>
+        In normal mode, a fact simply treats worlds as possible or
+        impossible. However sometimes you can infer more: for example, if you learn
+        by Periscope that the Fern was used to make an unknown Plus potion, the only
+        alchemical that you can outright eliminate is the triple-Minus, but it is more likely
+        that the Fern is the triple-Plus than anything else. Check the Bayes Mode
+        box if you want this extra information taken into account.
         <br/>
         <br/>
         <h4>The Table:</h4>
-        Each cell tells you the fraction of remaining worlds in which its ingredient maps to its
-        alchemical (rounded to the nearest percentage point).
+        Each cell tells you the probability its ingredient maps to its
+        alchemical (rounded to the nearest percentage point). This is simply the fraction
+        of remaining worlds that have that mapping (possibly weighted by Bayes Mode facts).
         </Dialog>
       </div>
     )
