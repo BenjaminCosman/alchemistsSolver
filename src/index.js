cbac017ab003531837dc38abe09c2af70d8bd701
Benjamin Cosman <bcosman@caltech.edu>
2016-12-24 16:18:25 -0800

    adds basic routing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {hashHistory, Router, Route} from 'react-router'
 import App from './App';
 import './index.css';
 
+function FooApp() {
+  return <div>Routing successful!</div>
+}
+
 ReactDOM.render(
-  <App />,
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+    <Route path="/blah" component={FooApp}/>
+  </Router>,
   document.getElementById('root')
 );
