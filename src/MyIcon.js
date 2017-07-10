4565f75bcc2e435ef32b4a129e98ea84438ae1b3
Benjamin Cosman <bcosman@caltech.edu>
2017-07-10 10:11:04 -0700

    icons smaller on small screens

--- src/MyIcon.js
+++ src/MyIcon.js
@@ -1,12 +1,48 @@
 import React from 'react'
 import {Image} from 'react-native'
+import {createStyles, minWidth} from 'react-native-media-queries';
+
+const base = {
+  icon: {
+    width: 18,
+    height: 18,
+  }
+}
+
+const bigger = {
+  icon: {
+    width: 30,
+    height: 30,
+  }
+}
+
+// let styleList = [base]
+// let size = 18
+// for (let i = 450; i < 600; i += 100) {
+//   size += 12
+//   styleList.push(minWidth(i, {
+//     icon: {
+//       width: size,
+//       height: size,
+//     }
+//   }))
+// }
+//
+// const styles = createStyles(
+//   ...styleList
+// );
+
+const styles = createStyles(
+  base,
+  minWidth(450, bigger),
+)
 
 function MyIcon(props) {
   return <Image
     resizeMode="contain"
-    style={{width: 30, height: 30}}
+    style={styles.icon}
     source={require('../images/' + props.imageDir + '/' + props.name + '.png')}
   />
 }
 
-export {MyIcon}
+export {MyIcon, styles}
